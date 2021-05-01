2dc19ecb9a93d2843b7bd4e68a637c097c5632b2
Melfsberg <69590682+Melfsberg@users.noreply.github.com>
2021-05-01 23:50:19 +0200

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
 from tkinter import *
-import serial, time
+import serial, time, threading, pickle
 import serial.tools.list_ports
-import pickle
 
 tidpunkt=['18:00','18:05','18:10','18:15']
 accvol=[0,0,0,0]
@@ -10,9 +9,10 @@ maxtid=[10,10,10,10]
 extravol=2
 extratid=10
 flagga=[1,1,1,1]
-
-buttons = ['0','1','2','3','4','5','6','7','8','9',':','<']
+tid_=0
 time1 = ''
+
+debug=0
 
 comlist = serial.tools.list_ports.comports()
 connected = []
@@ -21,38 +21,13 @@ for element in comlist:
 print("Connected COM ports: " + str(connected))
 
 if len(connected)>0:
-    ser=serial.Serial(str(connected[0]))
-
-gui= Tk()
-
-def keyboard():
-    
-    varRow = 410
-    varColumn = 0
-
-    for button in buttons:
-        command = lambda x=button: select(x)
-        Button(gui,text= button,width=3,height=2,command=command,font=("",18)).place(x=varColumn,y=varRow)
-        varColumn +=58
-
-def select(value):
-
-    focus=gui.focus_get()
-    
-    if (str(focus)=='.!entry6' or str(focus)=='.!entry10' or str(focus)=='.!entry14' or str(focus)=='.!entry18'):
-        return
-
-    if value == "<" :
-        focus.delete(0,END)
-    else :
-        focus.insert(END,value)        
-
+    ser=serial.Serial(str(connected[0]))      
+        
 def save_settings(file_name):
     global accvol,vol,maxtid,tidpunkt,extravol,extratid
     
     with open(file_name, 'wb') as file:
         pickle.dump([accvol,vol,maxtid,tidpunkt,extravol,extratid], file)    
-
 
 def load_settings(file_name):
     global accvol,vol,maxtid,tidpunkt,extravol,extratid
@@ -62,208 +37,288 @@ def load_settings(file_name):
             [accvol,vol,maxtid,tidpunkt,extravol,extratid]=pickle.load(file)
     except:
         print('no file')
-        
-def apply():
-    global accvol,vol,maxtid,tidpunkt,extravol,extratid
-    
-    save_settings('temp.dat')    
-    
-    for ent in [ent_tid1, ent_tid2, ent_tid3, ent_tid4]:    
-        if len(ent.get())!=5:
-            reset()        
-            return        
-        elif (int(ent.get()[0:2])>23 or int(ent.get()[0:2])<00 or
-                int(ent.get()[3:5])>59 or int(ent.get()[3:5])<00) :
-            reset()        
-            return
-    
-    
-    for i,a,v,m,t in zip(range(0,5), [ent_avl1,ent_avl2,ent_avl3,ent_avl4],[ent_vol1,ent_vol2,ent_vol3,ent_vol4],
-                         [ent_mtd1,ent_mtd2,ent_mtd3,ent_mtd4],[ent_tid1,ent_tid2,ent_tid3,ent_tid4]):
-                
-        accvol[i]=int(a.get())
-        vol[i]=(v.get())
-        maxtid[i]=(m.get())
-        tidpunkt[i]=(t.get())
-        
-    extravol=ent_extravol.get()
-    extratid=ent_extramtd.get()
-    
-    save_settings('settings.dat')
-    updategui()
-    
-def cancel():    
-    load_settings('temp.dat')
-    save_settings('settings.dat')
-    updategui()
 
-def reset():
-    global accvol
-    
-    accvol=[0,0,0,0]    
-    save_settings('settings.dat')
-    updategui()
-
-def updategui():
-    global accvol,vol,maxtid,tidpunkt,extravol,extratid
-   
-    for i,a,v,m,t in zip(range(0,5), [ent_avl1,ent_avl2,ent_avl3,ent_avl4],[ent_vol1,ent_vol2,ent_vol3,ent_vol4],
-                         [ent_mtd1,ent_mtd2,ent_mtd3,ent_mtd4],[ent_tid1,ent_tid2,ent_tid3,ent_tid4]):
-                
-        a.delete(0,END)
-        a.insert(0,accvol[i])   
-        v.delete(0,END)
-        v.insert(0,vol[i])        
-        m.delete(0,END)
-        m.insert(0,maxtid[i])
-        t.delete(0,END)
-        t.insert(0,tidpunkt[i])
-
-    ent_extravol.delete(0,END)
-    ent_extravol.insert(0,extravol)
-    ent_extramtd.delete(0,END)
-    ent_extramtd.insert(0,extratid)
-
-def tick():
-    global time1
-
-    time2 = time.strftime('%H:%M:%S')
-    if time2 != time1:
-        time1 = time2
-        clock.config(text=time2)
-    clock.after(200, tick)
-
-def check():
-    global vol, maxtid, tidpunkt,flagga
+def check():    
+    global vol, maxtid, tidpunkt,flagga,gui
     
     for n in range(0,4):
         if tidpunkt[n]==time.strftime('%H:%M') and flagga[n]:
-            vattna(n+1,vol[n],maxtid[n])
+            threading.Thread(vattna(n+1,vol[n],maxtid[n])).start()
+            gui.updategui()
             flagga[n]=0
     
         elif flagga[n]==0:
             tid=tidpunkt[n]
             if (int(tid[3:5]))+5 % 60 < int(time.strftime('%M')):
                 flagga[n]=1
+                    
+    threading.Timer(1, check).start()               
                 
-    clock.after(1000, check)
 
 def vattna(slinga,volym,tid):
     
-    global accvol
+    global accvol,tid_,debug
     cmd=str(slinga) + ',' + str(volym) + ',' + str(tid) + '\r'
+    volume=0
+    vol_cur=int(accvol[slinga-1])
+  
+    gui.b_clr('#BB0000',slinga)
+
+
+
+    if not(debug):
     
-    if 'ser' in globals():
-        ser.write(cmd.encode('ascii'))
-    
+        ser.write(cmd.encode('ascii'))    
         while True:
             tmp=ser.readline()
             tmp=tmp.decode('ascii')
             tmp=tmp.split(',')
             channel=int(tmp[0])
             volume=int(tmp[1])
-            #tid=int(tmp[2])
-                
+            tid_=int(tmp[2])
+            accvol[slinga-1]=vol_cur+volume
+            gui.updategui()
+            time.sleep(.2)
+                            
             if channel==-1:         
-                break
-      
-        accvol[slinga-1]=accvol[slinga-1]+(volume)
-        updategui()
-
+                break     
+    
     else:
-        time.sleep(2)
-        accvol[slinga-1]=accvol[slinga-1]+2
-        updategui()
+        while True:
+            tid_+=1
+            volume+=1
+            accvol[slinga-1]=vol_cur+volume
+            gui.updategui()
+            if int(tid_)>=int(tid):
+                break
+            time.sleep(1)
         
-    save_settings('settings.dat')
     
-def on_closing():    
-    gui.destroy()
-    exit()
-
-# GUI Design
-
-fnts=13;bfts=9;ewdt=6;bhgt=2;bwdt=10;timx=100;timlx=10;timy=5
-row0=15;row1=40;row2=80;row3=150;row4=220;row5=290;row6=360;row7=350;row8=420
-col0=10;col1=90;col2=190;col3=290;col4=390;col5=490;col6=530;ecl1=500;ecl2=600;colb1=10;colb2=140;colb3=270
+    save_settings('settinpgs.dat')
+    tid_=0
+    gui.b_clr('#D9D9D9',slinga)
+    gui.updategui()    
     
-Label(gui, text="Aktuell tid: ",font=("", fnts)).place(x=timlx,y=timy)
-clock=Label(gui,font=("", fnts))
-clock.place(x=timx,y=timy)
+class MainGUI:
+    def __init__(self):
+        
+        self.gui = Tk()
+                    
+        fnts=18;bfts=12;lfnts=12;ewdt=5;bhgt=2;bwdt=10;timx=110;timlx=10;timy=5
+        row0=15;row1=40;row2=80;row3=150;row4=220;row5=290;row7=350
+        col0=10;col1=90;col2=190;col3=290;col4=390;col6=530;ecl1=500;ecl2=600;colb1=10;colb2=140;colb3=270
+            
+        Label(self.gui, text="Aktuell tid: ",font=("", lfnts)).place(x=timlx,y=timy)
+        self.clock=Label(self.gui,font=("", lfnts))
+        self.clock.place(x=timx,y=timy)
+        
+        self.ent_vattnar=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts+7),bg='#BDBDBD')
+        self.ent_vattnar.place(x=col6,y=row7)
+        
+        Button(self.gui, text="Applicera", command = self.apply,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb1, y=row7)
+        Button(self.gui, text="Ångra", command = self.cancel,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb3, y=row7)
+        Button(self.gui, text="Reset", command = self.reset,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb2, y=row7)
 
-Button(gui, text="Applicera", command = apply,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb1, y=row7)
-Button(gui, text="Ångra", command = cancel,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb3, y=row7)
-Button(gui, text="Reset", command = reset,  height = bhgt, width = bwdt,font=("", bfts)).place(x=colb2, y=row7)
+        self.Bv1=Button(self.gui, text="Vattna 1",
+                command= lambda: [threading.Thread(target=vattna,args=[1,self.ent_extravol.get(),self.ent_extramtd.get()]).start()],
+                   height = bhgt, width = bwdt,font=("", bfts))
+        self.Bv1.place(x=col6, y=row2)
 
-Button(gui, text="Vattna 1",command= lambda: vattna(1,ent_extravol.get(),ent_extramtd.get()),  height = bhgt, width = bwdt,font=("", bfts)).place(x=col6, y=row2)
-Button(gui, text="Vattna 2", command= lambda: vattna(2,ent_extravol.get(),ent_extramtd.get()),  height = bhgt, width = bwdt,font=("", bfts)).place(x=col6, y=row3)
-Button(gui, text="Vattna 3", command= lambda: vattna(3,ent_extravol.get(),ent_extramtd.get()),  height = bhgt, width = bwdt,font=("", bfts)).place(x=col6, y=row4)
-Button(gui, text="Vattna 4", command= lambda: vattna(4,ent_extravol.get(),ent_extramtd.get()),  height = bhgt, width = bwdt,font=("", bfts)).place(x=col6, y=row5)
+        self.Bv2=Button(self.gui, text="Vattna 2",
+                command= lambda:[threading.Thread(target=vattna,args=[2,self.ent_extravol.get(),self.ent_extramtd.get()]).start()],
+                  height = bhgt, width = bwdt,font=("", bfts))
+        self.Bv2.place(x=col6, y=row3)
+       
+        self.Bv3=Button(self.gui, text="Vattna 3",
+                command= lambda:[threading.Thread(target=vattna,args=[3,self.ent_extravol.get(),self.ent_extramtd.get()]).start()],
+                  height = bhgt, width = bwdt,font=("", bfts))
+        self.Bv3.place(x=col6, y=row4)
+        
+        self.Bv4=Button(self.gui, text="Vattna 4",
+                command= lambda:[threading.Thread(target=vattna,args=[4,self.ent_extravol.get(),self.ent_extramtd.get()]).start()],
+                  height = bhgt, width = bwdt,font=("", bfts))
+        self.Bv4.place(x=col6, y=row5)
 
-Label(gui,text="Volym dl",font=("", fnts)).place(x=col1,y=row1)
-Label(gui,text="Maxtid s",font=("", fnts)).place(x=col2,y=row1)
-Label(gui,text="Tid hh:mm",font=("", fnts)).place(x=col3,y=row1)
-Label(gui,text="Acc vol dl",font=("", fnts)).place(x=col4,y=row1)
-Label(gui,text="Volym dl",font=("", fnts)).place(x=ecl1,y=row0)
-Label(gui,text="Maxtid s",font=("", fnts)).place(x=ecl2,y=row0)
+        Label(self.gui,text="Volym dl",font=("", lfnts)).place(x=col1,y=row1)
+        Label(self.gui,text="Maxtid s",font=("", lfnts)).place(x=col2,y=row1)
+        Label(self.gui,text="Tid hh:mm",font=("", lfnts)).place(x=col3,y=row1)
+        Label(self.gui,text="Tol vol dl",font=("", lfnts)).place(x=col4,y=row1)
+        Label(self.gui,text="Volym dl",font=("", lfnts)).place(x=ecl1,y=row0)
+        Label(self.gui,text="Maxtid s",font=("", lfnts)).place(x=ecl2,y=row0)
 
-ent_extravol=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_extravol.place(x=ecl1,y=row1)
-ent_extramtd=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_extramtd.place(x=ecl2,y=row1)
+        self.ent_extravol=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_extravol.place(x=ecl1,y=row1)
+        self.ent_extramtd=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_extramtd.place(x=ecl2,y=row1)
 
-Label(gui, text="Slinga 1",font=("", fnts)).place(x=col0,y=row2)
-ent_vol1=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_vol1.place(x=col1,y=row2)
-ent_mtd1=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_mtd1.place(x=col2,y=row2)
-ent_tid1=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_tid1.place(x=col3,y=row2)
-ent_avl1=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
-ent_avl1.place(x=col4,y=row2)
+        Label(self.gui, text="Slinga 1",font=("", lfnts)).place(x=col0,y=row2)
+        self.ent_vol1=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_vol1.place(x=col1,y=row2)
+        self.ent_mtd1=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_mtd1.place(x=col2,y=row2)
+        self.ent_tid1=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_tid1.place(x=col3,y=row2)
+        self.ent_avl1=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
+        self.ent_avl1.place(x=col4,y=row2)
 
-Label(gui, text="Slinga 2",font=("", fnts)).place(x=col0,y=row3)
-ent_vol2=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_vol2.place(x=col1,y=row3)
-ent_mtd2=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_mtd2.place(x=col2,y=row3)
-ent_tid2=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_tid2.place(x=col3,y=row3)
-ent_avl2=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
-ent_avl2.place(x=col4,y=row3)
+        Label(self.gui, text="Slinga 2",font=("", lfnts)).place(x=col0,y=row3)
+        self.ent_vol2=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_vol2.place(x=col1,y=row3)
+        self.ent_mtd2=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_mtd2.place(x=col2,y=row3)
+        self.ent_tid2=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_tid2.place(x=col3,y=row3)
+        self.ent_avl2=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
+        self.ent_avl2.place(x=col4,y=row3)
 
-Label(gui, text="Slinga 3",font=("", fnts)).place(x=col0,y=row4)
-ent_vol3=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_vol3.place(x=col1,y=row4)
-ent_mtd3=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_mtd3.place(x=col2,y=row4)
-ent_tid3=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_tid3.place(x=col3,y=row4)
-ent_avl3=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
-ent_avl3.place(x=col4,y=row4)
+        Label(self.gui, text="Slinga 3",font=("", lfnts)).place(x=col0,y=row4)
+        self.ent_vol3=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_vol3.place(x=col1,y=row4)
+        self.ent_mtd3=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_mtd3.place(x=col2,y=row4)
+        self.ent_tid3=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_tid3.place(x=col3,y=row4)
+        self.ent_avl3=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
+        self.ent_avl3.place(x=col4,y=row4)
 
-Label(gui, text="Slinga 4",font=("", fnts)).place(x=col0,y=row5)
-ent_vol4=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_vol4.place(x=col1,y=row5)
-ent_mtd4=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_mtd4.place(x=col2,y=row5)
-ent_tid4=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts))
-ent_tid4.place(x=col3,y=row5)
-ent_avl4=Entry(gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
-ent_avl4.place(x=col4,y=row5)
+        Label(self.gui, text="Slinga 4",font=("", lfnts)).place(x=col0,y=row5)
+        self.ent_vol4=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_vol4.place(x=col1,y=row5)
+        self.ent_mtd4=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_mtd4.place(x=col2,y=row5)
+        self.ent_tid4=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts))
+        self.ent_tid4.place(x=col3,y=row5)
+        self.ent_avl4=Entry(self.gui,width=ewdt,justify=RIGHT,font=("", fnts),bg='#EDEDED')
+        self.ent_avl4.place(x=col4,y=row5)
 
-for b in [ent_extravol, ent_extramtd, ent_tid1, ent_vol1, ent_mtd1, ent_tid2, ent_vol2, ent_mtd2,
-    ent_tid3, ent_vol3, ent_mtd3, ent_tid4, ent_vol4, ent_mtd4]:
-    b.bind("<Button-1>", lambda e: keyboard())
+        for b in [self.ent_extravol, self.ent_extramtd, self.ent_tid1, self.ent_vol1, self.ent_mtd1, self.ent_tid2, self.ent_vol2, self.ent_mtd2,
+            self.ent_tid3, self.ent_vol3, self.ent_mtd3, self.ent_tid4, self.ent_vol4, self.ent_mtd4]:
+            b.bind("<Button-1>", lambda e: self.keyboard())
+        
+        self.gui.geometry("720x480+0+0")
+        #self.gui.attributes("-fullscreen", True)
+        
+        self.keyboard()
 
-gui.geometry("680x480+0+0")
-gui.protocol("WM_DELETE_WINDOW", on_closing)
-#gui.attributes("-fullscreen", True)
+    def tick(self):
+        global time1
 
-tick()
+        time2 = time.strftime('%H:%M:%S')
+        if time2 != time1:
+            time1 = time2
+            self.clock.config(text=time2)
+        self.clock.after(200, self.tick)
+    
+    def updategui(self):
+        global accvol,vol,maxtid,tidpunkt,extravol,extratid,tid_
+
+        for i,a,v,m,t in zip(range(0,5), [self.ent_avl1,self.ent_avl2,self.ent_avl3,self.ent_avl4],
+                             [self.ent_vol1,self.ent_vol2,self.ent_vol3,self.ent_vol4],
+                         [self.ent_mtd1,self.ent_mtd2,self.ent_mtd3,self.ent_mtd4],
+                             [self.ent_tid1,self.ent_tid2,self.ent_tid3,self.ent_tid4]):
+
+            a.delete(0,END)
+            a.insert(0,accvol[i])   
+            v.delete(0,END)
+            v.insert(0,vol[i])        
+            m.delete(0,END)
+            m.insert(0,maxtid[i])
+            t.delete(0,END)
+            t.insert(0,tidpunkt[i])
+            
+        self.ent_extravol.delete(0,END)
+        self.ent_extravol.insert(0,extravol)
+        self.ent_extramtd.delete(0,END)
+        self.ent_extramtd.insert(0,extratid)
+        
+        self.ent_vattnar.delete(0,END)
+        self.ent_vattnar.insert(0,tid_)
+        
+    def keyboard(self):
+        buttons = ['0','1','2','3','4','5','6','7','8','9',':','<']
+        varRow = 410
+        varColumn = 5
+
+        for button in buttons:
+            command = lambda x=button: self.select(x)
+            Button(self.gui,text= button,width=2,height=2,command=command,font=("",18)).place(x=varColumn,y=varRow)
+            varColumn +=59
+
+    def select(self,value):
+
+        focus=self.gui.focus_get()
+        print(focus)
+        
+        
+        if (str(focus)=='.!entry7' or str(focus)=='.!entry11' or str(focus)=='.!entry15' or str(focus)=='.!entry19'):
+            return
+        
+        if value == "<" :
+            focus.delete(0,END)
+        else :
+            focus.insert(END,value)
+        
+    def apply(self):
+        global accvol,vol,maxtid,tidpunkt,extravol,extratid
+    
+        save_settings('temp.dat')    
+    
+        for ent in [self.ent_tid1, self.ent_tid2, self.ent_tid3, self.ent_tid4]:    
+            if len(ent.get())!=5:
+                self.reset()        
+                return       
+            
+            elif (int(ent.get()[0:2])>23 or int(ent.get()[0:2])<00 or
+                int(ent.get()[3:5])>59 or int(ent.get()[3:5])<00) :
+                self.reset()        
+                return    
+    
+        for i,a,v,m,t in zip(range(0,5), [self.ent_avl1,self.ent_avl2,self.ent_avl3,self.ent_avl4],
+                             [self.ent_vol1,self.ent_vol2,self.ent_vol3,self.ent_vol4],
+                         [self.ent_mtd1,self.ent_mtd2,self.ent_mtd3,self.ent_mtd4],
+                             [self.ent_tid1,self.ent_tid2,self.ent_tid3,self.ent_tid4]):                
+            accvol[i]=int(a.get())
+            vol[i]=(v.get())
+            maxtid[i]=(m.get())
+            tidpunkt[i]=(t.get())
+        
+        extravol=self.ent_extravol.get()
+        extratid=self.ent_extramtd.get()
+    
+        save_settings('settings.dat')
+        self.updategui()
+    
+    def b_clr(self,colr,slinga):
+        
+        if slinga==1:
+            self.Bv1.configure(bg=colr)
+        elif slinga==2:
+            self.Bv2.configure(bg=colr)
+        elif slinga==3:
+            self.Bv3.configure(bg=colr)
+        elif slinga==4:
+            self.Bv4.configure(bg=colr)
+
+        
+    def cancel(self):    
+        load_settings('temp.dat')
+        save_settings('settings.dat')
+        self.updategui()
+          
+    def reset(self):
+        global accvol
+    
+        accvol=[0,0,0,0]    
+        save_settings('settings.dat')
+        self.updategui()
+        
+    def run(self):
+        self.gui.mainloop()
+
 load_settings('settings.dat')
-updategui()
-keyboard()
 
+gui=MainGUI()
+gui.tick()
+gui.updategui()
 check()
-gui.mainloop()
+gui.run()
